80157a3e77e5631b2ac196ea1326fb00b5ca9a7f
Roman Bobrik <romanbobrik@tut.by>
2023-02-12 15:18:13 +0100

    added calculation of work hours

--- src/components/calc/calculator.tsx
+++ src/components/calc/calculator.tsx
@@ -1,3 +1,5 @@
+import type { ICurrency } from "@/lib/currencies";
+
 import {
   $,
   component$,
@@ -9,13 +11,15 @@ import {
 import { $translate as t } from "qwik-speak";
 import { convert, getSettings } from "@/lib/calc";
 import { getCurrencyByCountry } from "@/lib/countries";
-import { ICurrency } from "@/lib/currencies";
-import { Loader } from "~/components/ui";
+import { Loader } from "@/components/ui";
 
 export const Calculator = component$(() => {
+  const showWorkTime = useSignal(false);
   const state = useStore({
     amount: 0,
     calculated: 0,
+    workTime: 0,
+    hourRate: 0,
     myCurrency: {} as ICurrency | undefined,
     foreignCurrency: {} as ICurrency | undefined,
   });
@@ -26,6 +30,8 @@ export const Calculator = component$(() => {
     const settings = getSettings();
     state.myCurrency = getCurrencyByCountry(settings.myCountry);
     state.foreignCurrency = getCurrencyByCountry(settings.whereAmI);
+    state.hourRate = settings.hourRate;
+    showWorkTime.value = settings.calcHours;
 
     inputRef.value?.focus();
   });
@@ -38,11 +44,30 @@ export const Calculator = component$(() => {
         state.foreignCurrency?.code as string,
         state.myCurrency?.code as string
       )) || 0;
+    state.workTime = state.hourRate ? state.calculated / state.hourRate : 0;
   });
 
   const handleChange = $((event: InputEvent) => {
     state.amount = +(event.target as HTMLInputElement).value;
   });
+
+  const calculated = (
+    <div className="form-block text-center text-4xl space-x-2">
+      <span>=</span>
+      <span>{state.calculated.toFixed(2)}</span>
+      <span>{state.myCurrency?.code}</span>
+    </div>
+  );
+
+  const workTime = (
+    <div className="form-block text-center text-2xl space-x-2">
+      <span>{t("app.or@@Or")}</span>
+      <span class="font-bold">{state.workTime.toFixed(2)}</span>
+      <span>
+        {t("app.hours@@hour(s)")} {t("app.of@@of")} {t("app.work@@work")}
+      </span>
+    </div>
+  );
 
   return (
     <div class="mx-auto md:w-1/2 space-y-8">
@@ -59,16 +84,16 @@ export const Calculator = component$(() => {
           onInput$={handleChange}
         />
       </div>
+
       {resource.loading ? (
         <div class="text-center">
           <Loader />
         </div>
       ) : (
-        <div class="form-block text-center text-4xl space-x-2">
-          <span>=</span>
-          <span>{state.calculated.toFixed(2)}</span>
-          <span>{state.myCurrency?.code}</span>
-        </div>
+        <>
+          {calculated}
+          {showWorkTime.value && workTime}
+        </>
       )}
     </div>
   );
